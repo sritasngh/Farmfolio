fb0261a9f2b5e353ed7c857986b6b6727e35bcbe
Sarita Singh <saritasingh.0425@gmail.com>
2023-06-13 13:52:32 +0530

    Fill the template

--- index.js
+++ index.js
@@ -34,20 +34,47 @@ const slugify=require('slugify');
 // console.log('Will read a file!');
 
 //////////////////////////////////////////////////////////////////////////////////////////
+const replaceTemplate=(temp, product)=>{
+    let output=temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output=output.replace(/{%ID%}/g, product.id);
+    output=output.replace(/{%IMAGE%}/g, product.image);
+    output=output.replace(/{%FROM%}/g, product.from);
+    output=output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output=output.replace(/{%QUANTITY%}/g, product.quantity);
+    output=output.replace(/{%PRICE%}/g, product.price);
+    if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    output=output.replace(/{%DESCRIPTION%}/g, product.description);
+    return output;
+}
+// Loading the templates once so that it doesn't have to do each request
+const tempOverview=fs.readFileSync(`${__dirname}/templates/template_overview.html`,'utf-8');
+const tempProduct=fs.readFileSync(`${__dirname}/templates/template_product.html`,'utf-8');
+const tempCard=fs.readFileSync(`${__dirname}/templates/template_card.html`,'utf-8');
+
 // Creating a simple web Server
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj=JSON.parse(data);
 
 const server=http.createServer((req, res)=>{
     const pathName=req.url;
+
+    // Overview page
     if(pathName==='/' || pathName==='/overview'){
-        res.end('This is the OVERVIEW!');
+        const cardsHtml=dataObj.map(el=>replaceTemplate(tempCard, el));
+        const output=tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+    
+    // Product page
     } else if(pathName==='/product'){
         res.end('This is the PRODUCT!');
+
+    // API
     } else if(pathName==='/api'){
         res.writeHead(200,{
             'Content-type':'application/json'});
         res.end(data);
+    
+    // Not found
     } else{
         res.writeHead(404,{
             'Content-type':'text/html'
