3fb91f39aa42332d7f3190b8051c7077ab8deecd
Sarita Singh <saritasingh.0425@gmail.com>
2023-06-13 14:28:07 +0530

    Parsing varibles from url, learn to create and export a module

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const url=require('url');
 const slugify=require('slugify');
 
 // our own modules
+const replaceTemplate=require('./modules/replaceTemplate');
 
 //////////////////////////////////////////////////////////////////////////////////////
 //Files
@@ -34,18 +35,6 @@ const slugify=require('slugify');
 // console.log('Will read a file!');
 
 //////////////////////////////////////////////////////////////////////////////////////////
-const replaceTemplate=(temp, product)=>{
-    let output=temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output=output.replace(/{%ID%}/g, product.id);
-    output=output.replace(/{%IMAGE%}/g, product.image);
-    output=output.replace(/{%FROM%}/g, product.from);
-    output=output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output=output.replace(/{%QUANTITY%}/g, product.quantity);
-    output=output.replace(/{%PRICE%}/g, product.price);
-    if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    output=output.replace(/{%DESCRIPTION%}/g, product.description);
-    return output;
-}
 // Loading the templates once so that it doesn't have to do each request
 const tempOverview=fs.readFileSync(`${__dirname}/templates/template_overview.html`,'utf-8');
 const tempProduct=fs.readFileSync(`${__dirname}/templates/template_product.html`,'utf-8');
@@ -56,20 +45,26 @@ const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj=JSON.parse(data);
 
 const server=http.createServer((req, res)=>{
-    const pathName=req.url;
+    const {query, pathname}=url.parse(req.url, true);
 
     // Overview page
-    if(pathName==='/' || pathName==='/overview'){
+    if(pathname==='/' || pathname==='/overview'){
+        res.writeHead(200, {'content-type':'text/html'});
+
         const cardsHtml=dataObj.map(el=>replaceTemplate(tempCard, el));
         const output=tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
     
     // Product page
-    } else if(pathName==='/product'){
-        res.end('This is the PRODUCT!');
+    } else if(pathname==='/product'){
+        res.writeHead(200, {'content-type':'text/html'});
+
+        const product=dataObj[query.id];
+        const output=replaceTemplate(tempProduct, product);
+        res.end(output);
 
     // API
-    } else if(pathName==='/api'){
+    } else if(pathname==='/api'){
         res.writeHead(200,{
             'Content-type':'application/json'});
         res.end(data);
---
+++ modules/replaceTemplate.js
@@ -0,0 +1,12 @@
+module.exports=(temp, product)=>{
+    let output=temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output=output.replace(/{%ID%}/g, product.id);
+    output=output.replace(/{%IMAGE%}/g, product.image);
+    output=output.replace(/{%FROM%}/g, product.from);
+    output=output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output=output.replace(/{%QUANTITY%}/g, product.quantity);
+    output=output.replace(/{%PRICE%}/g, product.price);
+    if(!product.organic) output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    output=output.replace(/{%DESCRIPTION%}/g, product.description);
+    return output;
+}
